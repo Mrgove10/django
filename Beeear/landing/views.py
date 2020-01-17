d979a8d1cab4ca8c10116cae0bf00dd7a0784a93
Adrien RICHARD <richardadrien0@gmail.com>
2020-01-17 11:29:19 +0100

    added little page

--- Beeear/landing/views.py
+++ Beeear/landing/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Little
 # Create your views here.
 
 
@@ -11,3 +12,14 @@ def index(request):
         "name": "adrien"
     }  # to passe variables in teh view
     return HttpResponse(template.render(context, request))
+
+
+def littlepage(request,id):
+    littleVar = Little.objects.get(pk=id)
+    if littleVar != None:
+        template = loader.get_template('landing/littlepage.html')
+        context = {
+            "little": littleVar
+        }
+        return HttpResponse(template.render(context, request))
+    
