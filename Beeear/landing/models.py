94f3a9a69bd3b5a0f513175fdb1adf27f614d200
Adrien RICHARD <richardadrien0@gmail.com>
2020-01-17 09:58:32 +0100

    model

--- Beeear/landing/models.py
+++ Beeear/landing/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
+
+
+class little(models.Model):
+    name = models.CharField(),
+    weight = models.PositiveIntegerField(),
+    user = models.ForeignKey(User)
--- Beeear/landing/views.py
+++ Beeear/landing/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+
+def index(request):
+    template = loader.get_template('landing/index.html')
+    context = {
+        "name": "adrien"
+    }  # to passe variables in teh view
+    return HttpResponse(template.render(context, request))
